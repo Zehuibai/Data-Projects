6291428c762f61adafc52d5c6d4baa6c85662c6e
Zehuibai <56628775+Zehuibai@users.noreply.github.com>
2021-10-31 15:31:07 +0100

    20211031

---
+++ Python/Python_Learn_Update.py
@@ -0,0 +1,11 @@
+import numpy as np
+np.random.seed(12345)
+np.set_printoptions(precision=4, suppress=True)
+
+print('Hello world')
+
+
+
+import numpy as np
+data = {i : np.random.randn() for i in range(7)}
+data
